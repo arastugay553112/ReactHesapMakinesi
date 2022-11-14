65a142c169a3da38f89abe8c2a0ad3078531d995
tugay aras <kes.ici@hotmail.com>
2022-11-14 09:18:47 +0300

    my initial commit

--- src/index.js
+++ src/index.js
@@ -4,43 +4,89 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const scaleNames={
-  c:'Santigrat',
-  f:'fahrenayt'
+const scaleNames = {
+  c: 'santigrat',
+  f: 'fahrenayt'
 };
-class SicaklikGirisi extends React.Component{
-  constructor(props){
+
+function cevirSantigrat(fahrenayt){
+  return (fahrenayt-32) * 5 / 9;
+}
+function cevirFahrenayt(santigrat){
+  return (santigrat * 9 / 5) + 32;
+}
+
+function tryCeviri(sicaklik,convert){
+  const giris=parseFloat(sicaklik);
+  if(Number.isNaN(giris)){
+    return '';
+  }
+  const cikis=convert(giris);
+  const rounded=Math.round(cikis * 1000)/1000;
+  return rounded.toString();
+}
+
+function Kaynama(props){
+if(props.santigrat >=100){
+  return <p>kaynadı</p>
+}
+return <p>kaynamadı</p>
+}
+
+class SicaklikGirisi extends React.Component {
+  constructor(props) {
     super(props);
-    this.handleChange=this.handleChange.bind(this);
-    this.state={sicaklik: ''};
+    this.handleChange = this.handleChange.bind(this);
   }
-  handleChange(event){
-    this.setState({
-sicaklik: event.target.value
-    });
+
+  handleChange(e) {
+    this.props.onSicaklikCahnge(e.target.value);
   }
-  render(){
-    const sicaklik=this.state.sicaklik;
-    const scale=this.props.scale;
+
+  render() {
+    const sicaklik = this.props.sicaklik;
+    const scale = this.props.scale;
     return (
       <fieldset>
-        <legend>Sıcaklığı Girin: {scaleNames[scale]}:</legend>
-        <input value={sicaklik} onChange={this.handleChange} />
+        <legend>Sıcaklık Girin: {scaleNames[scale]}:</legend>
+        <input value={sicaklik}
+               onChange={this.handleChange} />
       </fieldset>
     );
   }
 }
-class Derece extends React.Component{
-  render(){
+
+class Derece extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleSantigratChange= this.handleSantigratChange.bind(this);
+    this.handleFahrenaytChange= this.handleFahrenaytChange.bind(this);
+    this.state={sicaklik: '',scale:'c'};
+  }
+
+  handleSantigratChange(sicaklik){
+this.setState({scale:'c',sicaklik});
+  }
+  handleFahrenaytChange(sicaklik){
+    this.setState({scale:'f',sicaklik});
+      }
+
+  render() {
+
+    const scale=this.state.scale;
+    const sicaklik=this.state.sicaklik;
+    const santigrat=scale==='f' ? tryCeviri(sicaklik,cevirSantigrat) : sicaklik;
+    const fahrenayt=scale==='c' ? tryCeviri(sicaklik,cevirFahrenayt) : sicaklik;
+
     return (
       <div>
-        <SicaklikGirisi scale="c" />
-        <SicaklikGirisi scale="f" />
+        <SicaklikGirisi scale="c" sicaklik={santigrat} onSicaklikCahnge={this.handleSantigratChange} />
+        <SicaklikGirisi scale="f"  sicaklik={fahrenayt} onSicaklikCahnge={this.handleFahrenaytChange} />
+        <Kaynama santigrat={parseFloat(santigrat)} />
       </div>
     );
   }
 }
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
